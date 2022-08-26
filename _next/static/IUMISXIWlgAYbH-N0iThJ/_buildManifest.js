8b4b9a97e1c081a0278dff8df9ae6956b10865f4
applemango <applemango@users.noreply.github.com>
2022-08-26 06:08:16 +0000

    deploy: a69a9ef8ebbea25ed0580097f8a486402868fd83

---
+++ _next/static/IUMISXIWlgAYbH-N0iThJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f2f9ef729cc61c00.css","static/chunks/pages/index-6261cca5492df861.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
