ff90806fea968eedd1219f569dc4267d6fa2dfa6
applemango <apple.mangoooo97@gmail.com>
2022-08-29 08:02:26 +0900

    fix

--- components/other/mouse.tsx
+++ components/other/mouse.tsx
@@ -13,7 +13,8 @@ type Props = {
 const Mouse = ({ children }:Props) => {
     const [mousePositionX, setMousePositionX] = useState(0)
     const [mousePositionY, setMousePositionY] = useState(0)
-    const [target, setTarget]:any = useState()
+    const [show, setShow] = useState(true)
+    const [target, setTarget] = useState(undefined)
     const [mobile, setMobile] = useState(false)
     const isSpecialHover = () => {
         return target == "A"
@@ -32,18 +33,25 @@ const Mouse = ({ children }:Props) => {
                 <div
                     className={styles.main}
                     onMouseMove={(e: any) => {
+                        setShow(false)
                         if(!(window.innerWidth <= e.clientX + 50)) {
                             setMousePositionX(e.clientX)
                         }
                         if(!(window.innerHeight <= e.clientY + 50)) {
                             setMousePositionY(e.clientY)
                         }
+                        if(!(window.innerWidth <= e.clientX + 50) && !(window.innerHeight <= e.clientY + 50)) {
+                            setShow(true)
+                        }
                         setTarget(e.target.nodeName)
                     }}
+                    onMouseLeave={() => setShow(false)}
                 >{children}</div>
-                <div className={`${styles.cursor} ${isSpecialHover() ? styles.active : ""}`} style={{
-                    transform: `translateX(${mousePositionX}px) translateY(${mousePositionY}px)`
-                }} />
+                { show && (
+                    <div className={`${styles.cursor} ${isSpecialHover() ? styles.active : ""}`} style={{
+                        transform: `translateX(${mousePositionX}px) translateY(${mousePositionY}px)`
+                    }} />
+                )}
             </div>
             )}
         </div>
