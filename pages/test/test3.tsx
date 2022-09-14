0e68232a12f87693c7a9b2bd83365b133783757f
applemango <apple.mangoooo97@gmail.com>
2022-09-14 13:15:04 +0900

    fix

--- pages/test/test3.tsx
+++ pages/test/test3.tsx
@@ -18,7 +18,7 @@ const TEST: NextPage = () => {
             setA(Number(router.query.a))
             setB(Number(router.query.b))
             setC(Number(router.query.c))
-            console.log(main(Number(router.query.a),Number(router.query.b),Number(router.query.c)))
+            //console.log(main(Number(router.query.a),Number(router.query.b),Number(router.query.c)))
         }
         if(router.query.type) {
             setType(true)
@@ -36,17 +36,48 @@ const TEST: NextPage = () => {
             let top = one[0]+two[0]
             let top2 = one[0]-two[0]
             let bottom = three[0]
-            console.log("A")
+            let top_M = top < 0
+            let top2_M = top2 < 0
+            let top_ROF = ROF(Math.abs(top),bottom)
+            let top2_ROF = ROF(Math.abs(top2),bottom)
+            if(top_ROF[0] == top2_ROF[0] && top_ROF[1] == top2_ROF[1]) {
+                return <BlockMath math={`x= ${top_ROF[0]} \\over ${top_ROF[1]}`}/>
+            } else {
+                let a = `{${top_ROF[0]} \\over ${top_ROF[1]}}`
+                let b = `{${top2_ROF[0]} \\over ${top2_ROF[1]}}`
+                return <div>
+                    <BlockMath math={`
+                        x= ${top_M ? "-" : ""} ${top_ROF[1] == 1 ? top_ROF[0] : a}
+                        ,${top2_M ? "-" : ""} ${top2_ROF[1] == 1 ? top2_ROF[0] : b}`}
+                    />
+                </div>
+                //x= ${top_M ? "-" : ""} {${top_ROF[0]} \\over ${top_ROF[1]}}
+                //,${top2_M ? "-" : ""} {${top2_ROF[0]} \\over ${top2_ROF[1]}}
+            }
+            return <BlockMath math={`x=0`} />
             return [
                 `${top}/${bottom}`
                 ,`${top2}/${bottom}`
             ]
         } else {
-            return 
+            let two_SROF = SROF(two[0])
+            if(two_SROF.length == 1) {
+                two_SROF = [1,two[0]]
+            }
+            let all_ROF = ROF3(one[0],two_SROF[0],three[0])
+            one[0] = all_ROF[0]
+            two_SROF[0] = all_ROF[1]
+            three[0] = all_ROF[2]
+            //console.log(`x=${one[0]}±root_${two_SROF[0]}_${two_SROF[1]}/${three[0]}`)
+            //three[0] = ROF()
+            //console.log(SROF(20))
+            //console.log(two_SROF)
+            return <BlockMath math={`x= {${one[0]}\\pm ${two_SROF[0] == 1 ? "" : two_SROF[1]}\\sqrt{${two_SROF[1]}} \\over ${three[0]}}`} />
         }
     }
     return (
         <div className={styles.main}>
+            {main(a,b,c)}
         </div>
     )
 }
