b0792f61613f3449f3c53edd46f5e48b0d269e4f
applemango <apple.mangoooo97@gmail.com>
2022-11-12 09:59:09 +0900

    fix main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,40 +5,30 @@ import styles from './styles/index.module.scss'
 
 import Mouse from '../components/other/mouse'
 
+const Main = ({children}:{children: any}) => {return <div className={styles.main}>{children}</div>}
+const Links = ({children}:{children: any}) => {return <div className={styles.cards}>{children}</div>}
+const A = ({href, children}:{href: string, children: string}) => {return <Link href={href}><a><div className={styles.card}><p>{children}</p></div></a></Link>}
+const H1 = ({children}:{children: string}) => {return <div className={styles.title} style={{marginTop:75}}><p>{children}</p></div>}
+const Title = () => {return <div className={styles.title}><h1>UI Components</h1><p>Components Collection</p></div>}
+Main.Links = Links
+Main.A = A
+Main.H1 = H1
+Main.Title = Title
+
 const Home: NextPage = () => {
-    return (
-        <div className={styles.main}>
-            <div className={styles.title}>
-                <h1>UI Components</h1>
-                <p>Components Collection</p>
-            </div>
-            <div className={styles.cards}>
-                <div className={styles.card}>
-                    <Link href="/c/input">
-                        <a>input</a>
-                    </Link>
-                </div>
-                <div className={styles.card}>
-                    <Link href="/c/github"><a>github</a></Link>
-                </div>
-                <div className={styles.card}>
-                    <Link href="/c/header"><a>header</a></Link>
-                </div>
-            </div>
-            
-            <div className={styles.title} style={{marginTop:150}}>
-                <p>page Collection</p>
-            </div>
-            <div className={styles.cards}>
-                <div className={styles.card}>
-                    <Link href="/old/pages/p"><a>test page 1</a></Link>
-                </div>
-                <div className={styles.card}>
-                    <Link href="/old/test/test3?a=0&b=0&c=0"><a>test page 2 (quadratic equation</a></Link>
-                </div>
-            </div>
-        </div>
-    )
+    return <Main>
+        <Main.Title />
+        <Main.Links>
+            <Main.A href="/c/input">input</Main.A>
+            <Main.A href="/c/github">github</Main.A>
+            <Main.A href="/c/header">header</Main.A>
+        </Main.Links>
+        <Main.H1>page Collection</Main.H1>
+        <Main.Links>
+            <Main.A href="/old/pages/p">test page 1</Main.A>
+            <Main.A href="/old/test/test3?a=0&b=0&c=0">test page 2 (quadratic equation</Main.A>
+        </Main.Links>
+    </Main>
 }
 
 export default Home
